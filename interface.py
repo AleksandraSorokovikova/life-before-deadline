03d40c11bb23019aa5a3f177941e2f95947d3556
Alexandra <alexandraroze2000@gmail.com>
2022-10-19 13:54:43 +0300

    seconf iteration

--- interface.py
+++ interface.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+import random
+
 
 win = tk.Tk()  # создание окна
 win.title('Lesson')  # название
@@ -7,6 +9,19 @@ h = 500
 w = 600
 x = 500
 y = 100
+
+def button_1_func():
+    numbers = [1, 2, 3, 4, 5, 6]
+    random.shuffle(numbers)
+    label_1.config(text=str(numbers[0]))
+    label_2.config(text=str(numbers[1]))
+    label_3.config(text=str(numbers[2]))
+    label_4.config(text=str(numbers[3]))
+    label_5.config(text=str(numbers[4]))
+    label_6.config(text=str(numbers[5]))
+
+
+
 
 label_1 = tk.Label(win,
                    bg='#ede1ed',
@@ -43,12 +58,25 @@ label_5 = tk.Label(win,
                    padx=20,
                    pady=20,
                    relief=tk.RAISED)
+label_6 = tk.Label(win,
+                   bg='#ede1ed',
+                   fg='white',
+                   font=('Arial', 20, 'bold'),
+                   padx=20,
+                   pady=20,
+                   relief=tk.RAISED)
 
 label_1.grid(row=0, column=0)
 label_2.grid(row=0, column=1)
 label_3.grid(row=1, column=0)
 label_4.grid(row=1, column=1)
 label_5.grid(row=2, column=0)
+
+button_1 = tk.Button(win,
+                     text='Бросить кубики',
+                     command=button_1_func)
+
+button_1.grid(row=3, column=0)
 
 
 #
