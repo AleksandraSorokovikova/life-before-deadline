e65f272dc050b871941f9171420b191106c97517
Deuqz <ddemon2002@mail.ru>
2022-10-19 15:41:53 +0300

    Add table with results

--- interface.py
+++ interface.py
@@ -4,7 +4,7 @@ import poker
 win = tk.Tk()  # создание окна
 win.title('Lesson')  # название
 h = 1000
-w = 600
+w = 900
 x = 500
 y = 100
 
@@ -208,6 +208,14 @@ button_2 = tk.Button(win,
 button_2.grid(row=3, column=2, stick='e')
 
 
+table = [[tk.Label(win,
+                   bg='#ede1ed',
+                   fg='white',
+                   font=('Arial', 20, 'bold'),
+                   padx=10,
+                   pady=10,
+                   relief=tk.RAISED) for _ in range(3)] for _ in range(12)]
+
 def start():
     label_1.grid(row=0, column=0, stick='w')
     label_2.grid(row=0, column=1, stick='w')
@@ -220,6 +228,46 @@ def start():
     label_32.grid(row=1, column=2, stick='e')
     label_42.grid(row=1, column=3, stick='e')
     label_52.grid(row=2, column=3, stick='e')
+
+    table[0][1].config(text='Player 1')
+    table[0][2].config(text='Player 2')
+    table[1][0].config(text='1')
+    table[2][0].config(text='2')
+    table[3][0].config(text='3')
+    table[4][0].config(text='4')
+    table[5][0].config(text='5')
+    table[6][0].config(text='6')
+    table[7][0].config(text='pair')
+    table[8][0].config(text='two pair')
+    table[9][0].config(text='triangle')
+    table[10][0].config(text='small street')
+    table[11][0].config(text='big street')
+    table[1][1].config(text='0')
+    table[2][1].config(text='0')
+    table[3][1].config(text='0')
+    table[4][1].config(text='0')
+    table[5][1].config(text='0')
+    table[6][1].config(text='0')
+    table[7][1].config(text='0')
+    table[8][1].config(text='0')
+    table[9][1].config(text='0')
+    table[10][1].config(text='0')
+    table[11][1].config(text='0')
+    table[1][2].config(text='0')
+    table[2][2].config(text='0')
+    table[3][2].config(text='0')
+    table[4][2].config(text='0')
+    table[5][2].config(text='0')
+    table[6][2].config(text='0')
+    table[7][2].config(text='0')
+    table[8][2].config(text='0')
+    table[9][2].config(text='0')
+    table[10][2].config(text='0')
+    table[11][2].config(text='0')
+
+    for i, row in enumerate(table):
+        for j, cell in enumerate(row):
+            cell.grid(row=i + 4, column=j, stick='w')
 
     win.geometry(f'{h}x{w}+{x}+{y}')  # размер окна
     win.resizable(False, False)  # возможность изменять режим окна в реальном времени
